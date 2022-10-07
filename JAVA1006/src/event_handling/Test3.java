f2c6bd4f242a52b6b1c7470c8eb2abce4f6a3f53
Son JungMin <106176701+jm981001@users.noreply.github.com>
2022-10-07 16:55:30 +0900

    init commit

---
+++ JAVA1006/src/event_handling/Test3.java
@@ -0,0 +1,17 @@
+package event_handling;
+
+public class Test3 {
+
+	public Test3() {
+		showFrame();
+	}
+	public void showFrame() {
+		
+	}
+	public static void main(String[] args) {
+		new Test3(); 
+	}
+	class MyMemberActionListener {
+		
+	}
+}
